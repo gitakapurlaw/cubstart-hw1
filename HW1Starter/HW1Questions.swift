402d3449a4c9a8ba6a5de2448e8f428307c9a8a3
romakapoor <gita.kapur@gitakapurlaw.com>
2026-09-22 21:48:19 -0700

    complete HW1

--- HW1Starter/HW1Questions.swift
+++ HW1Starter/HW1Questions.swift
@@ -16,8 +16,16 @@ class HW1Questions {
     ///   - filenames: An array of file names
     ///   - count: Target length of file name (excluding the file type)
     /// - Returns: An array of file names whose excluded file type length matches `count`.
-    func getFileNames(for filenames: [String], withCount count: Int) -> [String] {
-        return []
+    func getFileNames(for filenames: [String], withCount count: Int) -> [String] { var result: [String] = []
+        for filename in filenames {
+        let parts = filename.split(separator: ".")
+            let name = parts [0]
+            
+            if name.count == count {
+            result.append(filename)
+        }
+        }
+        return result
     }
     
     
@@ -38,6 +46,23 @@ class HW1Questions {
     ///   - escapeIndex: The ending index
     /// - Returns: A boolean. True if we can escape. False otherwise.
     func canEscape(withDirections directions: [[Direction]], startingIndex: Int, escapeIndex: Int) -> Bool {
+        for instruction in directions {
+        if instruction.contains(.up) || instruction.contains(.down) {
+                continue
+        }
+        var currentIndex = startingIndex
+
+        for direction in instruction {
+            if direction == .right {
+                currentIndex += 1
+            } else if direction == .left {
+                currentIndex -= 1
+            }
+        }
+        if currentIndex == escapeIndex {
+            return true
+        }
+    }
         return false
     }
     
